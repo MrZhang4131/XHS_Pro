efd7ee85c680e8be37e9bee222ca3ab82b833d86
31283 <3128385708@qq.com>
2024-04-10 21:58:48 +0800

    修复了模型

---
+++ XHS_Pro/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Humanizer;
+using System.ComponentModel.DataAnnotations;
+
+namespace XHS_Pro.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public DateTime created { get; set; }
+        public DateTime updated { get; set; }
+        public string? username { get; set; }
+        public string? password { get; set; }
+
+        public string? phone { get; set; }
+        public string? headphoto { get; set; }
+        public int gender { get; set; }
+        public int age { get; set; }
+        public string? introduction { get; set; }
+        public string? usertype { get; set; }
+    }
+}
---
+++ XHS_Pro/Models/Zan.cs
@@ -0,0 +1,10 @@
+﻿namespace XHS_Pro.Models
+{
+    public class Zan
+    {
+        public int Id { get; set; }
+        public int userid { get; set; }
+        public int noteid { get; set; }
+        public int enable {  get; set; }
+    }
+}
